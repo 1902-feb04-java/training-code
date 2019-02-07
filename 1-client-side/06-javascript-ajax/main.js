fdfece07dcfc5602b6a474c55605a67d2f69aabb
Nicholas Escalona <nicholas.escalona@gmail.com>
2019-02-07 13:58:07 -0600

    finish ajax with XHR example.

--- 1-client-side/06-javascript-ajax/main.js
+++ 1-client-side/06-javascript-ajax/main.js
@@ -15,9 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('3');
 
         // we'll write a getJoke to make a request to the joke API
-        let jokeText = getJoke();
-
-        jokeHeader.textContent = jokeText;
+        getJoke(text => {
+            // this code runs when the request returns successfully
+            jokeHeader.textContent = text;
+        });
     });
 
     // runs fourth
@@ -27,28 +28,52 @@ document.addEventListener('DOMContentLoaded', () => {
 // runs second (the handler for DOMContentLoaded has not run yet!)
 console.log('5');
 
+
 // 24153
 // 15243
 // 12345
 
-function getJoke() {
+function getJoke(onSuccess, onFailure = console.log) {
     // this object is provided by the browser to let us do AJAX
     // AJAX stands for Asynchronous JavaScript and XML
     //   but what it means in practice is, HTTP requests via JavaScript.
     let xhr = new XMLHttpRequest();
 
+    // define what to do as the request-response lifecycle proceeds
     xhr.addEventListener('readystatechange', () => {
         console.log(`readyState: ${xhr.readyState}`);
 
+        // readyState starts at 0 and counts upwards
         if (xhr.readyState === 4) {
-            // 4 means, we've recieved the response
+            // 4 means, we've received the response
             let responseJSON = xhr.response;
             console.log(responseJSON);
 
             // HTTP response status 200-299 indicates success.
             if (xhr.status >= 200 && xhr.status < 300) {
+                // responseJSON should contain the data I requested.
+                console.log('success');
 
+                // JSON.parse to deserialize JSON.
+                // JSON.stringify to serialize JSON.
+
+                let responseObj = JSON.parse(responseJSON);
+                let text = responseObj.value.joke;
+                onSuccess(text);
+            } else {
+                // some kind of error (e.g. 404 not found)
+                // responseJSON should contain details about error
+                console.log('failure');
+                if (onFailure !== undefined) {
+                    onFailure(responseJSON);
+                }
             }
         }
     });
+
+    // set up the request to be made
+    xhr.open('get', 'http://api.icndb.com/jokes/random/');
+
+    // send the request
+    xhr.send();
 }
